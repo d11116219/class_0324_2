e18f3541259d85c517a9c33250341201660ae02a
d11116219 <126222904+d11116219@users.noreply.github.com>
2026-03-24 16:13:14 +0800

    Add files via upload

---
+++ class_0324_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Manager
+{
+    public readonly DateTime Startup;
+
+    public Manager()
+    {
+        Startup = DateTime.Now;
+    }
+
+    public DateTime GetStartup()
+    {
+        return Startup;
+    }
+}
